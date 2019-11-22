23ea058ad97a1e9e58d171dd2375837cdc92320c
ParisCheng <cmingwh@cn.ibm.com>
2019-11-22 10:47:20 +0800

    Update config

--- src/main/java/com/ibm/fullstack/ResourceApplication.java
+++ src/main/java/com/ibm/fullstack/ResourceApplication.java
@@ -2,8 +2,10 @@ package com.ibm.fullstack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 class ResourceApplication {
--- src/main/java/com/ibm/fullstack/service/TrainingService.java
+++ src/main/java/com/ibm/fullstack/service/TrainingService.java
@@ -1,10 +1,12 @@
 package com.ibm.fullstack.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.ibm.fullstack.entity.Training;
 import com.ibm.fullstack.repository.TrainingRepository;
 
+@Service("trainingService")
 public class TrainingService {
 	@Autowired
     private TrainingRepository trainingRepository;
